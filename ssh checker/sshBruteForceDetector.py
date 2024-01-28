a855d635a21b37a190523fb8cb2a8b6c2d7574b4
Cristian Hernandez <c-o.hernandez@hotmail.com>
2024-01-28 17:33:25 +0100

    example from class

--- ssh checker/sshBruteForceDetector.py
+++ ssh checker/sshBruteForceDetector.py
@@ -1,17 +1,56 @@
 #!/usr/bin/env python3
 import systemd.journal
-import time
+from pprint import pprint
+
+time_window = 60
+threshold = 3
+
+def compute_failure_event(session, event):
+  print("[!] invalid login detected")
+
+  event_message = event['MESSAGE']
+  src_ip = event_message.split()[5]
+  user = event_message.split()[3]
+
+
+  if src_ip not in session:
+     
+     #create new session entry
+     session[src_ip] = {
+        'hits' : 1,
+        'timestamp' : event['_SOURCE_REALTIME_TIMESTAMP'],
+        'user' : user
+     }
+
+  else:
+     delta = event['_SOURCE_REALTIME_TIMESTAMP'] - session[src_ip]['timestamp']
+
+     if delta.seconds > time_window:
+        # reset hit counter
+        session[src_ip]['hits'] = 1
+     else:
+        #within time window:
+        session[src_ip]['hits'] += 1
+
+        #evaluate hit counter
+        if session[src_ip]['hits'] > threshold: 
+           print ('[!] Brute-Force attempt detected. Within the last ' + str(time_window) + 'seconds, an attacker tried at least' + str(threshold) + 'times to log in with an invalid pw!')
+
+        #update timestamp
+        session[src_ip]['timestamp'] = event['_SOURCE_REALTIME_TIMESTAMP']
+
 
 def main():
   print("[+] Starting SSH Brute Force Detector.")
-  
-  failedAttempts = {}
   
   #open a connection to systemd jornal
   with systemd.journal.Reader() as journal:
     # seek to the end of the journal to get new entries
     journal.seek_tail()
     journal.get_previous()
+
+    #creating session variable
+    session = {}
 
     #loop throuth journal entries:
     while True:
@@ -22,39 +61,8 @@ def main():
           if 'sshd' in entry.get('SYSLOG_IDENTIFIER', ''):
               #if there is an entry that includes "Failed password for" in the "MESSAGE" entry, then..
               if "Failed password for" in entry["MESSAGE"]:
+                 compute_failure_event(session, entry)
                 
-                #splitting output into multiple words
-                parts = entry['MESSAGE'].split()
-                #getting the 6th word of the "MESSAGE" entry data
-                ipAddress = parts[5]
-                userName = parts[3]
-                #"MONOTONIC_TIMESTAMP" Monotonic time starts at an arbitrary point. It cannot move backwards. Finally, and most importantly, it is completely independent of the system clock
-                timeStamp = entry['__MONOTONIC_TIMESTAMP']
-
-
-
-                #############################CHECK THIS:
-                if userName in failedAttempts:
-                  # Update the timestamp and increment the count
-                  last_timestamp, count = failedAttempts[userName]
-                  if timeStamp - last_timestamp >= 60:
-                      count += 1
-                      failedAttempts[userName] = (timeStamp, count)
-
-                      # Check if the count exceeds 2 (3 failed attempts)
-                      if count == 3:
-                          print(f"The user {userName} with the IP Address {ipAddress} entered 3 wrong passwords in less than 1 minute.")
-                          # Add your alerting mechanism here
-                      else:
-                          # Reset count if the timestamp difference is more than 1 minute
-                          failedAttempts[userName] = (timeStamp, 1)
-                  else:
-                      # Add the user to the dictionary with initial values
-                      failedAttempts[userName] = (timeStamp, 1)
-                ##################################
-                      
-
-                      
       #exit programm when user clicks "CTRL+C"
       except KeyboardInterrupt:
         print("[+] Stopping SSH Brute Force Detector.")
